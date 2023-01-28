7b18ea59f3499abc3c069f14293525292ceaa0e2
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-28 14:49:10 -0300

    pagination in the user route

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/src/components/Pagination";
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
@@ -110,6 +111,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
