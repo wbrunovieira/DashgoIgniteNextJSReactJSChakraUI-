092ba8e5c9789135113f855aee44c5f2ab20942a
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-28 12:15:03 -0300

    created the Component Header, started the page Dasboard

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+
+import { theme } from '@/src/styles/themes'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+
+      <Component {...pageProps} />
+
+    </ChakraProvider>
+  )
+}
