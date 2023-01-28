d01d734f774a64746858edc9f2213f18b28ac9ed
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-28 11:04:54 -0300

    created the project, added ChakraUI and theme file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+
+import { theme } from '@/styles/themes'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+
+      <Component {...pageProps} />
+
+    </ChakraProvider>
+  )
 }
